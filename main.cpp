54c1d13df7c4378c81d059ed5878c6f8fee70db8
CDNGLM <ismaeldk7834@gmail.com>
2026-05-22 00:19:33 -0500

    Add cliente functionality and update product constructor

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "almacen.hpp"
 #include "producto.hpp"
+#include "cliente.hpp"
 
 using namespace std;
 
 int main()
 {
-    cProduct p1("Cacho", 2.5, 50), p2("Alfajor", 3.0, 30), p3("Trujillano", 1.5, 30);
+    cProduct p1("Cacho", 2.5, 50,1), p2("Alfajor", 3.0, 30,2), p3("Trujillano", 1.5, 30,3);
     cProduct l1 [3] ={p1, p2, p3};
     cAlmacen alma(l1, 3);
+    cCliente usuarioNuevo("Jose");
+    usuarioNuevo.realizarPedidos(alma);
 
-    alma.buscar_disponibilidad("Cacho", 15);
     return 0;
 }
