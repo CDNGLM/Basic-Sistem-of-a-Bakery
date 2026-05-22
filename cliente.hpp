5dafd2e37ffae752838826dbfa8eb74384a7e7e0
CDNGLM <ismaeldk7834@gmail.com>
2026-05-22 00:18:52 -0500

    Add cCliente class definition in cliente.hpp

---
+++ cliente.hpp
@@ -0,0 +1,19 @@
+#ifndef CLIENTE_HPP
+#define CLIENTE_HPP
+#include "almacen.hpp"
+
+
+class cCliente
+{
+protected:
+    char * name;
+public:
+    cCliente (const char * nombre = nullptr);
+    ~cCliente ();
+
+    int seleccionarProducto();
+    int seleccionarCantidad();
+    void realizarPedidos(cAlmacen l1);
+
+};
+
