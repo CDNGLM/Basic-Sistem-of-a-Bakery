22784f1bce8f4789f90ab454b822b7a886811f2a
CDNGLM <ismaeldk7834@gmail.com>
2026-05-24 19:43:38 -0500

    Refactor cCliente to inherit from cPersona
    Refactor cCliente class to inherit from cPersona and update realizarPedidos method signature.

--- cliente.hpp
+++ cliente.hpp
@@ -1,19 +1,14 @@
 #ifndef CLIENTE_HPP
 #define CLIENTE_HPP
 #include "almacen.hpp"
+#include "persona.hpp"
 
 
-class cCliente
+class cCliente : public cPersona
 {
-protected:
-    char * name;
 public:
     cCliente (const char * nombre = nullptr);
     ~cCliente ();
-
-    int seleccionarProducto();
-    int seleccionarCantidad();
-    void realizarPedidos(cAlmacen l1);
-
+    void realizarPedidos(cAlmacen &l1);
 };
-#endif
+#endif  
